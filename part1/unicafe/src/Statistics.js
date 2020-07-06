0cb1c740810c4fe5418aea622f3473d356bf5505
laitsky <vincent.diamond15@gmail.com>
2020-07-06 22:46:28 +0700

    1.11 done

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -1,26 +1,12 @@
 import React from 'react';
-//for commit 1.8 (unicafe step 3)
-//statistics component already used since exercises 1.6
-const Statistics = ({ data }) => {
-    const allFeedbacks = Object.values(data).reduce((a, b) => a + b);
-    const avgFeedbacks = (data.good - data.bad) / allFeedbacks;
-    const positiveFeedbacks = data.good / allFeedbacks;
 
-    if (allFeedbacks) {
-        return (
-            <div>
-                <h1>statistics</h1>
-                <p>good {data.good}</p>
-                <p>neutral {data.neutral}</p>
-                <p>bad {data.bad}</p>
-                <p>all {allFeedbacks}</p>
-                {(!isNaN(avgFeedbacks)) && <p>average {avgFeedbacks}</p>}
-                {(!isNaN(positiveFeedbacks)) && <p>positive {positiveFeedbacks}%</p>}
-            </div>
-        )
-    } else {
-        return <p>No feedback given</p>
-    }
+const Statistics = ({ text, value }) => {
+    return (
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
+    )
 }
 
 export default Statistics;
--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -16,19 +16,46 @@ const App = () => {
     bad
   }
 
+  const allFeedbacks = Object.values(feedbackValues).reduce((a, b) => a + b);
+  const avgFeedbacks = (feedbackValues.good - feedbackValues.bad) / allFeedbacks;
+  const positiveFeedbacks = (feedbackValues.good / allFeedbacks) * 100;
+
   const addGoodFeedback = () => setGood(good + 1);
   const addNeutralFeedback = () => setNeutral(neutral + 1);
   const addBadFeedback = () => setBad(bad + 1);
 
-  return (
-    <div>
-      <h1>give feedback</h1>
-      <Button handleClick={addGoodFeedback} text="good" />
-      <Button handleClick={addNeutralFeedback} text="neutral" />
-      <Button handleClick={addBadFeedback} text="bad" />
-      <Statistics data={feedbackValues} />
-    </div>
-  )
+  if (allFeedbacks) {
+    return (
+      <div>
+        <h1>give feedback</h1>
+        <Button handleClick={addGoodFeedback} text="good" />
+        <Button handleClick={addNeutralFeedback} text="neutral" />
+        <Button handleClick={addBadFeedback} text="bad" />
+        <h1>statistics</h1>
+        <table>
+          <tbody>
+            <Statistics value={good} text="good" />
+            <Statistics value={neutral} text="neutral" />
+            <Statistics value={bad} text="bad" />
+            <Statistics value={allFeedbacks} text="all" />
+            {(!isNaN(avgFeedbacks)) && <Statistics value={avgFeedbacks} text="average" />}
+            {(!isNaN(positiveFeedbacks)) && <Statistics value={positiveFeedbacks} text="positive" />}
+          </tbody>
+        </table>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h1>give feedback</h1>
+        <Button handleClick={addGoodFeedback} text="good" />
+        <Button handleClick={addNeutralFeedback} text="neutral" />
+        <Button handleClick={addBadFeedback} text="bad" />
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
