02c9d3964b347c9b9f132c3d24145c2280c24d68
laitsky <vincent.diamond15@gmail.com>
2020-07-06 22:19:22 +0700

    1.8 done

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+//for commit 1.8 (unicafe step 3)
+//statistics component already used since exercises 1.6
 const Statistics = ({ data }) => {
     const allFeedbacks = Object.values(data).reduce((a, b) => a + b);
     const avgFeedbacks = (data.good - data.bad) / allFeedbacks;
     const positiveFeedbacks = data.good / allFeedbacks;
-    
+
     return (
         <div>
             <h1>statistics</h1>
