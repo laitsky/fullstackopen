16f4ed6b7966ec92bdfcab6d6d642fb9eb190e17
laitsky <vincent.diamond15@gmail.com>
2020-07-06 22:18:00 +0700

    1.7 done

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 const Statistics = ({ data }) => {
+    const allFeedbacks = Object.values(data).reduce((a, b) => a + b);
+    const avgFeedbacks = (data.good - data.bad) / allFeedbacks;
+    const positiveFeedbacks = data.good / allFeedbacks;
+    
     return (
         <div>
             <h1>statistics</h1>
             <p>good {data.good}</p>
             <p>neutral {data.neutral}</p>
             <p>bad {data.bad}</p>
+            <p>all {allFeedbacks}</p>
+            <p>average {avgFeedbacks}</p>
+            {(!isNaN(positiveFeedbacks)) && <p>positive {positiveFeedbacks}%</p>}
         </div>
     )
 }
