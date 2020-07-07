e196a2d2a4cba0900bc345919db4f3a874bd0ece
laitsky <vincent.diamond15@gmail.com>
2020-07-07 20:25:18 +0700

    2.3 done

--- part2/courseinfo/src/Content.js
+++ part2/courseinfo/src/Content.js
@@ -3,6 +3,8 @@ import Part from './Part';
 import Total from './Total';
 
 const Content = ({parts}) => {
+    //already calcualting the sum of exercises using reduce in 2.2
+    //for commit "2.3" purpose only.
     const totalExercises = parts.reduce((total, part) => part.exercises + total, 0);
     return (
         <div>
