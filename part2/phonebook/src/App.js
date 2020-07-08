fac482072cbd368178702661e05644389f78f0f1
laitsky <vincent.diamond15@gmail.com>
2020-07-08 20:35:58 +0700

    2.11 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Artho Hellas', number: '040-1234567' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ]);
+    const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
     const [filter, setFilter] = useState('');
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(res => setPersons(res.data))
+    }, []);
 
     const handleNameChange = event => setNewName(event.target.value);
     const handleNumberChange = event => setNewNumber(event.target.value);
