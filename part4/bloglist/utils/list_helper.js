6da6f76c53cdb3ae3555f9bd158f51653a485057
laitsky <vincent.diamond15@gmail.com>
2020-07-13 21:13:08 +0700

    4.5 done

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,4 +2,11 @@ const dummy = blogs => 1;
 
 const totalLikes = blogs => blogs.reduce((total, blog) => total + blog.likes, 0)
 
-module.exports = {dummy, totalLikes}
+const favoriteBlog = blogs => {
+    const target = Math.max(...blogs.map(blog => blog.likes))
+    const blog = blogs.find(blog => blog.likes === target)
+    const {title, author, likes} = blog
+    return {title, author, likes}
+}
+
+module.exports = {dummy, totalLikes, favoriteBlog}
