6739ac4cf6be01c4b964b96beb5e9bcc652c9d77
laitsky <vincent.diamond15@gmail.com>
2020-07-18 08:44:25 +0700

    4.21 done

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -37,8 +37,19 @@ blogsRouter.post("/", async (request, response) => {
 });
 
 blogsRouter.delete("/:id", async (req, res) => {
-  await Blog.findByIdAndRemove(req.params.id)
-  res.status(204).end()
+  const decodedToken = jwt.verify(req.token, process.env.SECRET)
+
+  if (!req.token || !decodedToken.id) {
+    return response.status(401).json({error: 'token missing or invalid'})
+  }
+
+  const blog = await Blog.findById(req.params.id)
+  if (blog.user.toString() === decodedToken.id.toString()) {
+    await blog.remove()
+    res.status(204).end()
+  } else {
+    res.status(401).end()
+  }
 })
 
 blogsRouter.put("/:id", async (req, res) => {
